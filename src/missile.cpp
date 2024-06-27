4f92809fcdd1d7d74245876b70619209516479ac
Earle Castledine <earlec@21csw.com>
2024-06-27 10:33:34 +0800

    angle

--- src/missile.cpp
+++ src/missile.cpp
@@ -1,10 +1,15 @@
 #include <missile.hpp>
 
+float angle(Vector2 a, Vector2 b) {
+    return atan2f(b.y - a.y, b.x - a.x);
+}
+
 bool rm::missile::update(float dt) {
     if (target != nullptr) {
         raylib::Vector2 p1 = {t.pos.z, t.pos.x};
         raylib::Vector2 p2 = {target->z, target->x};
-        auto a = p1.Angle(p2);
+        auto a = angle(p1, p2);
+
         auto rot_speed = 2.0f;
         if (a > t.rot.y) t.rot.y += rot_speed * dt;
         if (a < t.rot.y) t.rot.y -= rot_speed * dt;
